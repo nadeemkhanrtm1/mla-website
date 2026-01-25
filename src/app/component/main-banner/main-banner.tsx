2d7eb87e0545f134950ce71a230f705efea68730
Zoya Khan <thegreatzoyakhan@gmail.com>
2026-01-25 13:17:48 +0530

    Hero Banner Correct

---
+++ src/app/component/main-banner/main-banner.tsx
@@ -0,0 +1,28 @@
+const MainBanner = ({
+  bannerTitle,
+  subTitle,
+}: {
+  bannerTitle: string;
+  subTitle: string;
+}) => {
+  return (
+    <div className="py-20 md:py-28 bg-flag-color relative overflow-hidden">
+      <div className="container mx-auto px-4 relative z-10">
+        <div className="max-w-4xl mx-auto text-center">
+          <div className="inline-flex flex-col justify-start items-center gap-4">
+            <div className="self-stretch flex flex-col justify-start items-center gap-2">
+              <div className="self-stretch text-center justify-center text-white text-5xl font-semibold">
+                {bannerTitle}
+              </div>
+            </div>
+            <div className="text-center justify-start text-white text-xl font-medium leading-7">
+              {subTitle}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainBanner;
---
+++ src/app/interviews/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const InterviewAndArticle = () => {
+  return <div>InterviewAndArticle</div>;
+};
+
+export default InterviewAndArticle;
